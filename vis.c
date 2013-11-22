7b6656e619d1825873123d7eceaa6df83e36874f
Tom Fogal <tfogal@alumni.unh.edu>
2013-11-22 15:38:55 +0100

    Ignore children terminating.
    The simulation doesn't care / it's moved on by then.

--- vis.c
+++ vis.c
@@ -1,7 +1,9 @@
 #define _POSIX_C_SOURCE 200801L
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include "vis.h"
@@ -13,6 +15,10 @@ void launch_vis(const char* filename)
     fprintf(stderr, "Could not fork vis process: %d\n", errno);
     return;
   }
+  struct sigaction nocld;
+  memset(&nocld, 0, sizeof(struct sigaction));
+  nocld.sa_flags = SA_NOCLDWAIT;
+  sigaction(SIGCLD, &nocld, NULL);
   if(pid == 0) {
     unsetenv("LD_PRELOAD");
     execl("/usr/bin/pvbatch", "pvbatch", "--use-offscreen-rendering",
