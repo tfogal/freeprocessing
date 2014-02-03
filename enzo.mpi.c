941e84cfbc3bac401c0f3d2eefc5fad18f651c67
Tom Fogal <tfogal@alumni.unh.edu>
2014-02-03 22:06:39 +0100

    fix typos in comments

--- enzo.mpi.c
+++ enzo.mpi.c
@@ -14,12 +14,12 @@ exec(const char* fn, const void* buf, size_t n)
   for(size_t i=0; i < 64; ++i) {
     snprintf(fname, 256, "%zu-xvel.%zu", i, rank());
     /* try to create the file, but with O_EXCL: i.e. it *must* be created.
-     * if it actually creates, it we're done.  if it fails, keep spinning (up
-     * to 64times) as per the loop. */
+     * if it actually creates it, we're done.  if it fails, keep spinning (up
+     * to 64 times) as per the loop. */
     fp = fopen(fname, "wex");
     if(fp) { break; }
   }
-  if(fp == NULL) { return; } /* fuck it, dude.  ... Let's go bowling. */
+  if(fp == NULL) { return; } /* fuck it, Dude.  ... Let's go bowling. */
   assert(fp);
   if(fwrite(buf, 1, n, fp) != n) {
     WARN(enzo, "short write writing %zu-byte field %s!", n, fn);
