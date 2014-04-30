133c44ad4932524f75ce4dcb942f68aa7c30eccc
Tom Fogal <tfogal@alumni.unh.edu>
2014-04-30 15:34:08 +0200

    doc: editing, and create html outputs as well.

--- doc/bld.sh
+++ doc/bld.sh
@@ -1,1 +1,4 @@
-a2x -L -f pdf freeprocessor.adoc
+a2x --icons -L -f pdf freeprocessor.adoc || exit 1
+asciidoc -a icons freeprocessor.adoc || exit 1
+str="s,tfogal@sci.utah.edu,tfogal@sci,g"
+sed -i ${str} freeprocessor.html
