6c4f36b38e7703cb508e623f850813ee71ee4657
Tom Fogal <tfogal@alumni.unh.edu>
2014-05-02 16:15:24 +0200

    fix some typos/edits/etc. (thanks Jens!)

--- doc/bld.sh
+++ doc/bld.sh
@@ -1,4 +1,4 @@
 a2x --icons -L -f pdf freeprocessor.adoc || exit 1
 asciidoc -a icons freeprocessor.adoc || exit 1
-str="s,tfogal@sci.utah.edu,tfogal@sci,g"
+str="s,tfogal@sci.utah.edu,tfogal.at.uni-due,g"
 sed -i ${str} freeprocessor.html
--- processors/minmax/mminit.c
+++ processors/minmax/mminit.c
@@ -10,3 +10,13 @@ void
 finish(const char* fn) {
   printf("The data range of %s is: [%d:%d]\n", fn, minmax[0], minmax[1]);
 }
+void
+exec(const char* fn, const void* buf, size_t n) {
+  (void)fn;
+  const int* data = (const int*)buf;
+  const size_t elems = n / sizeof(int);
+  for(size_t i=0; i < elems; ++i) {
+    minmax[0] = data[i] < minmax[0] ? data[i] : minmax[0];
+    minmax[1] = data[i] > minmax[1] ? data[i] : minmax[1];
+  }
+}
